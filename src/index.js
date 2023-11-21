3cd698c8a36022ea02848cf819d1faca84b2e990
Mauro <mauromaio.dev@gmail.com>
2023-11-21 11:37:40 -0300

    feat: home import update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global-styles.css";
-
-import Home from "./templates/Home/Home";
+import { Home } from "./templates/Home/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
